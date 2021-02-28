246d65d28d9a18d1b2ce2c9b4ee7c55ae19c7450
denis lopez <lo0706032020@gmail.com>
2021-02-28 14:22:31 -0600

    Menu

---
+++ LaboratorioProgramacionUno/Vista/FrmMenu.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace LaboratorioProgramacionUno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnUsuario = new System.Windows.Forms.Button();
+            this.BtnAdmin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnUsuario
+            // 
+            this.BtnUsuario.Location = new System.Drawing.Point(12, 46);
+            this.BtnUsuario.Name = "BtnUsuario";
+            this.BtnUsuario.Size = new System.Drawing.Size(197, 102);
+            this.BtnUsuario.TabIndex = 0;
+            this.BtnUsuario.Text = "Usuario";
+            this.BtnUsuario.UseVisualStyleBackColor = true;
+            // 
+            // BtnAdmin
+            // 
+            this.BtnAdmin.Location = new System.Drawing.Point(239, 46);
+            this.BtnAdmin.Name = "BtnAdmin";
+            this.BtnAdmin.Size = new System.Drawing.Size(197, 102);
+            this.BtnAdmin.TabIndex = 1;
+            this.BtnAdmin.Text = "Administrador";
+            this.BtnAdmin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 209);
+            this.Controls.Add(this.BtnAdmin);
+            this.Controls.Add(this.BtnUsuario);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnUsuario;
+        private System.Windows.Forms.Button BtnAdmin;
+    }
+}
+
